63977bbd6eaa776d57fc39bd614894e56486eac9
Sumit Sharma <summitkumarsharma@gmail.com>
2020-06-06 20:42:51 +0530

    inserted args and kwargs

---
+++ tut41.py
@@ -0,0 +1,21 @@
+# def function_name_print(a, b, c, d, e):
+#     print(a, b, c, d, e)
+
+def funargs(normal, *args, **kwargs):
+    print(normal)
+    for item in args:
+        print(item)
+    print("\nNow I would Like to introduce some of our heroes")
+    for key, value in kwargs.items():
+        print(f"{key} is a {value}")
+
+
+# function_name_print("Harry", "Rohan", "Skillf", "Hammad", "Shivam")
+
+lst = ["Sumit", "Sohan", "Skillf", "Hammad",
+       "Shivam", "The programmer"]
+normal = "I am a normal Argument and the students are:"
+kw = {"Sohan": "Monitor", "Sumit": "Fitness Instructor",
+      "The Programmer": "Coordinator", "Shivam": "Cook"}
+funargs(normal, *lst, **kw)
+
